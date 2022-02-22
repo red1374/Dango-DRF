b53182835cf3877f1d9df9d9a5f25122f0a8e87c
Плешаков Сергей <main@pleshakov.org>
2022-02-22 15:19:16 +0500

    ADD. Add authors to standard admin panel

--- service/authors/models.py
+++ service/authors/models.py
@@ -10,3 +10,6 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.birthday_year})"
